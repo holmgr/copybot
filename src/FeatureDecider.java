23dd774536cba5b09d9a1cee7e24777d67d45483
Fredrik Wallström <frewa814@student.liu.se>
2016-11-24 12:36:19 +0100

    WIP in featuredecider, dont work. Classification always pick agent zero for some reason

--- src/FeatureDecider.java
+++ src/FeatureDecider.java
@@ -16,18 +16,19 @@ public class FeatureDecider {
 
     private ArrayList<String> featuresAvailableTraining;
 
-    private String[] trainingFiles = {"-b", "1", "featuresTrainModified.train"};
-    private String[] predictFiles = {"-b", "1", "featuresValidationModified.train", "featuresTrainModified.train.model", "probabilityForController.out"};
+    private String[] trainingFiles = {"-b", "1", "-q", "featuresTrainModified.train"};
+    private String[] predictFiles = {"-b", "1", "-q", "featuresValidationModified.train", "featuresTrainModified.train.model", "probabilityForController.out"};
     private double bestAcc = 0;
-    private double lowestAcc = 100.0;
+    private double accOnCurrLevel = 0;
     private boolean firstrun = true;
 
     public FeatureDecider() throws IOException {
         FileCombiner fc = new FileCombiner();
         initializeArray();
-        while (featuresAvailableTraining.size() > 0) {
+        while (!featuresAvailableTraining.isEmpty()) {
             int featureToRemove = -1;
             boolean changeFeature = false;
+            accOnCurrLevel = 0;
             for (int i = 0; i < featuresAvailableTraining.size(); i++) {
                 buildFeatureFile(i, "featuresTraining.txt", "featuresTrainModified.txt");
                 buildFeatureFile(i, "featuresValidation.txt", "featuresValidationModified.txt");
@@ -40,15 +41,18 @@ public class FeatureDecider {
                     featureToRemove = i;
                 }
             }
-            if(featureToRemove != -1) featuresAvailableTraining.remove(featureToRemove);
-            else break;
+            for(String str : featuresAvailableTraining){
+                System.out.println(str);
+
+            }
+            System.out.println("\n");
+            if(accOnCurrLevel >= bestAcc) featuresAvailableTraining.remove(featureToRemove);
+          //  else break;
+            System.out.println(accOnCurrLevel);
         }
 
-        for(String str : featuresAvailableTraining){
-            System.out.println(str);
 
-        }
-        System.out.println(bestAcc);
+
 
     }
 
@@ -62,17 +66,17 @@ public class FeatureDecider {
     private boolean changeFeature() throws IOException {
         BufferedReader feBr = new BufferedReader(new FileReader("result.out"));
         String accuracy = feBr.readLine();
-        if (firstrun){
+     /*   if (firstrun){
             bestAcc = Double.parseDouble(accuracy);
             firstrun = false;
-        }
+        } */
 
-        if(Double.parseDouble(accuracy) > bestAcc){
+    /*    if(Double.parseDouble(accuracy) > bestAcc){
             bestAcc = Double.parseDouble(accuracy);
         }
-
-        if(Double.parseDouble(accuracy) <= lowestAcc){
-            lowestAcc = Double.parseDouble(accuracy);
+*/
+        if(Double.parseDouble(accuracy) > accOnCurrLevel){
+            accOnCurrLevel = Double.parseDouble(accuracy);
             return true;
         }else
             return false;
