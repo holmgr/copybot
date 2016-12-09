bcafd0df7a40e8cda079a3c073670a521f2710a9
Yousif Touma <youto814@student.liu.se>
2016-12-09 10:22:06 +0100

    Remove unwanted features

--- src/controllers/singlePlayer/featureCollectingAgent/Agent.java
+++ src/controllers/singlePlayer/featureCollectingAgent/Agent.java
@@ -2,42 +2,24 @@ package controllers.singlePlayer.featureCollectingAgent;
 
 import controllers.singlePlayer.sampleMCTS.SingleMCTSPlayer;
 import core.game.*;
-import core.game.Event;
 import core.player.AbstractPlayer;
-import ontology.Types;
 import ontology.Types.ACTIONS;
 import tools.ElapsedCpuTimer;
-import tools.Vector2d;
-
-import java.awt.*;
 
 import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.util.*;
-import java.util.List;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Random;
-import java.util.SortedSet;
-import java.util.TreeSet;
-
-
 
 /**
  *
  */
 public class Agent extends AbstractPlayer {
-
     public int num_actions;
     public ACTIONS[] actions;
     // Features of a game, used for training set
     private HashMap<String, Double> features = new HashMap<>();
-    private Event lastEvent = null;
-    private boolean firstRun = true;
-    private int canShoot = 0;
     private final static String FILENAME = "features.txt";
 
     // Use MCTS for feature collection
@@ -71,8 +53,6 @@ public class Agent extends AbstractPlayer {
 	initFeatures();
 
 	//Collect features
-	Dimension dim = so.getWorldDimension();
-	double size = dim.getHeight()*dim.getWidth();
 	int blockSize = so.getBlockSize();
 	int canMoveVertically = 0;
 	int isUseAvailable = 0;
@@ -86,7 +66,6 @@ public class Agent extends AbstractPlayer {
 	}
 	features.put("isUseAvailable", isUseAvailable+0.0);
 	features.put("canMoveVertically", canMoveVertically +0.0);
-	features.put("worldSize", size);
 	features.put("blockSize", blockSize+0.0);
 
 	//mctsPlayer.init(so);
@@ -144,16 +123,8 @@ public class Agent extends AbstractPlayer {
 
     private void initFeatures(){
 	features.put("numNPCTypes", 0.0);
-	features.put("numNPC", 0.0);
-	features.put("numImmovableSprites", 0.0);
-	features.put("numMovableSprites", 0.0);
-	features.put("numPlayerSprites", 0.0);
 	features.put("numPortals", 0.0);
-	features.put("numPortalTypes", 0.0);
 	features.put("numTypesResources", 0.0);
-	features.put("isResourcesAvailable", 0.0);
-	features.put("numTypesResourcesAvatar", 0.0);
-	features.put("avatarHasResources", 0.0);
     }
 
     private void detectFeatures(StateObservation stateObs){
@@ -161,19 +132,9 @@ public class Agent extends AbstractPlayer {
 	detectPortalFeatures(portalTypes);
 	ArrayList<Observation>[] NPCTypes = stateObs.getNPCPositions();
 	detectNPCFeatures(NPCTypes);
-	ArrayList<Observation>[] immovableTypes = stateObs.getImmovablePositions();
-	ArrayList<Observation>[] movableTypes = stateObs.getMovablePositions();
-	ArrayList<Observation>[] spritesTypesByPlayer = stateObs.getFromAvatarSpritesPositions();
-	detectSpriteFeatures(immovableTypes, movableTypes, spritesTypesByPlayer);
 
-	Map<Integer, Integer> avatarResources = stateObs.getAvatarResources();
 	ArrayList<Observation>[] resources = stateObs.getResourcesPositions();
-	detectResourceFeatures(resources, avatarResources);
-
-	Set<Event> events = stateObs.getEventsHistory();
-	if (canShoot == 0) {
-	    tryDetectShootFeature(events);
-	}
+	detectResourceFeatures(resources);
     }
 
     /**
@@ -182,17 +143,11 @@ public class Agent extends AbstractPlayer {
      */
     private void detectPortalFeatures(List<Observation>[] portalTypes) {
 	double numPortals = 0.0;
-	double numPortalTypes = 0.0;
-
 	if (portalTypes != null) {
-	    numPortalTypes = portalTypes.length;
 	    for (List<Observation> portalType : portalTypes) numPortals += portalType.size();
 	}
 	if(numPortals > features.get("numPortals")){
 	    features.put("numPortals", numPortals);
-	}
-	if(numPortalTypes > features.get("numPortalTypes")){
-	    features.put("numPortalTypes", numPortalTypes);
 	}
     }
 
@@ -200,86 +155,22 @@ public class Agent extends AbstractPlayer {
      * Get the number of NPC types and the total number of NPC in the game.
      */
     private void detectNPCFeatures(List<Observation>[] NPCTypes) {
-	double numNPC = 0.0;
 	double numNPCTypes = 0.0;
 
 	if (NPCTypes != null) {
 	    numNPCTypes = NPCTypes.length;
-	    for (List<Observation> NPCType : NPCTypes) numNPC += NPCType.size();
 	}
-	if(numNPC > features.get("numNPC")){
-	    features.put("numNPC", numNPC);
-	}
+
 	if(numNPCTypes > features.get("numNPCTypes")){
 	    features.put("numNPCTypes", numNPCTypes);
 	}
     }
 
-    private void detectSpriteFeatures(List<Observation>[] immovableTypes, List<Observation>[] movableTypes,
-				      List<Observation>[] spritesTypesByPlayer) {
-	double numImmovableSprites = 0.0;
-	double numMovableSprites = 0.0;
-	double numPlayerSprites = 0.0;
-
-	// Get the number if types of immovable sprites.
-	if (immovableTypes != null) numImmovableSprites = immovableTypes.length;
-	if(numImmovableSprites > features.get("numImmovableSprites")) {
-	    features.put("numImmovableSprites", numImmovableSprites);
-	}
-
-	// Get the number of types of movable sprites (NOT NPC).
-	if (movableTypes != null) numMovableSprites = movableTypes.length;
-	if(numMovableSprites > features.get("numMovableSprites")){
-	    features.put("numMovableSprites", numMovableSprites);
-	}
-
-	// Get the number of types of sprites that are created by the player.
-	if (spritesTypesByPlayer != null) numPlayerSprites = spritesTypesByPlayer.length;
-	if(numPlayerSprites > features.get("numPlayerSprites")){
-	    features.put("numPlayerSprites", numPlayerSprites);
-	}
-    }
-
-    private void detectResourceFeatures(List<Observation>[] resources, Map<Integer, Integer> avatarResources ) {
+    private void detectResourceFeatures(List<Observation>[] resources) {
 	if (resources != null){
 	    int numTypesResources = resources.length;
-	    double isResourcesAvailable = numTypesResources == 0 ?
-					  0.0 : 1.0;
-	    if (isResourcesAvailable > features.get("isResourcesAvailable")) {
-		features.put("isResourcesAvailable", isResourcesAvailable);
-	    }
 	    if (numTypesResources > features.get("numTypesResources")){
 		features.put("numTypesResources", numTypesResources+0.0);
-	    }
-	}
-	int numTypesResourcesAvatar = avatarResources.size();
-	double avatarHasResources = numTypesResourcesAvatar == 0 ?
-				    0.0 : 1.0;
-	if (numTypesResourcesAvatar > features.get("numTypesResourcesAvatar")) {
-	    features.put("numTypesResourcesAvatar", numTypesResourcesAvatar+0.0);
-	}
-	if (avatarHasResources > features.get("avatarHasResources")) {
-	    features.put("avatarHasResources", avatarHasResources);
-	}
-    }
-
-    private void tryDetectShootFeature(Set<Event> events) {
-	if (firstRun && !events.isEmpty()) {
-	    checkEventsForCollision(events);
-	    firstRun = false;
-	} else if (!events.isEmpty()) {
-	    SortedSet<Event> newEvents = ((TreeSet)events).tailSet(lastEvent);
-	    checkEventsForCollision(newEvents);
-	}
-	features.put("canShoot", canShoot+0.0);
-    }
-
-    private void checkEventsForCollision(Set<Event> events){
-	for (Event event : events) {
-	    lastEvent = event;
-	    if (event.fromAvatar) {
-		canShoot = 1;
-		break;
 	    }
 	}
     }
