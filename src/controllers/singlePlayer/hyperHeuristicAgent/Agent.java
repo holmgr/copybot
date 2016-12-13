189611ebb346632ff5f80840fb1e20aeb1885dc1
holmgr <viktor.holmgren@gmail.com>
2016-12-13 09:21:39 +0100

    Fix ordering of feature vectors (maps are unordered lol :P), also fix indentation

--- src/controllers/singlePlayer/hyperHeuristicAgent/Agent.java
+++ src/controllers/singlePlayer/hyperHeuristicAgent/Agent.java
@@ -23,30 +23,26 @@ import java.util.Map;
  */
 public class Agent extends AbstractPlayer {
 
-    HashMap<Integer, String> controllersMap = new HashMap<Integer, String>() {{
-        put(0,"Yolobot");
-	put(1, "YBCriber");
-	put(2, "thorbjrn");
-	put(3, "NovTea");
-    }};
-
-    HashMap<Integer, Integer> classificationCertainyIndexMap = new HashMap<Integer, Integer>() {{
-	put(0, 1);
-    	put(1, 4);
-    	put(2, 2);
-    	put(3, 3);
-        }};
-
-    // Selected agent from portfolio
-    private AbstractPlayer chosenAgent = null;
-
     // Constants
     private final static int ARBITRARY_NUMBER = 47;
-
     // Filenames
     private final static String FEATURES_FILENAME = "featur.es";
     private final static String SVM_MODEL_FILENAME = "classify.model";
     private final static String CLASSIFIED_RESULT = "controller.ans";
+    HashMap<Integer, String> controllersMap = new HashMap<Integer, String>() {{
+        put(0,"Yolobot");
+        put(1, "YBCriber");
+        put(2, "thorbjrn");
+        put(3, "NovTea");
+    }};
+    HashMap<Integer, Integer> classificationCertainyIndexMap = new HashMap<Integer, Integer>() {{
+        put(0, 1);
+        put(1, 4);
+        put(2, 2);
+        put(3, 3);
+    }};
+    // Selected agent from portfolio
+    private AbstractPlayer chosenAgent = null;
 
     /**
      * Public constructor with state observation and time due.
@@ -55,66 +51,77 @@ public class Agent extends AbstractPlayer {
      */
     public Agent(StateObservation so, ElapsedCpuTimer elapsedTimer) throws IOException
     {
-	StateObservation obsCopy = so.copy();
-	final controllers.singlePlayer.featureCollectingAgent.Agent featureCollectingAgent =
-		new controllers.singlePlayer.featureCollectingAgent.Agent(obsCopy, elapsedTimer);
-	Map<String, Double> features = featureCollectingAgent.getFeatures();
-	String featureString = features.toString();
-	System.out.println(featureString);
+        StateObservation obsCopy = so.copy();
+        final controllers.singlePlayer.featureCollectingAgent.Agent featureCollectingAgent =
+                new controllers.singlePlayer.featureCollectingAgent.Agent(obsCopy, elapsedTimer);
+        Map<String, Double> features = featureCollectingAgent.getFeatures();
 
-	// write features to file for classification
-	// TODO check that the ordering of features is correct
-	try (Writer writer = new BufferedWriter(new OutputStreamWriter(
-		new FileOutputStream(FEATURES_FILENAME), "utf-8")))
-	{
-	    // write arbitrary number larger than number of classes for SVM to not take this as a training example
-	    writer.write(ARBITRARY_NUMBER + " ");
-	    String[] splittedOnSpace = featureString.split(" ");
-	    for (int i = 0; i < features.size(); i++) {
-		String[] splittedOnEqual = splittedOnSpace[i].split("=");
-		String[] splittedOnDot = splittedOnEqual[1].split("\\.");
-		String result = splittedOnDot[0];
-		if(i == 0) writer.write((i+1)+ ":" + result);
-		else writer.write(" "+ (i+1) + ":" + result);
-	    }
-	}
-	catch (Exception e){
-	    System.out.println(String.format("Got Exception: %s", e));
-	}
-	svm_predict.main(new String[]{ "-b", "1", FEATURES_FILENAME, SVM_MODEL_FILENAME, CLASSIFIED_RESULT });
-	int controllerClass = -1;
-	double classificationCertainty = -1.0;
-	try (BufferedReader reader = new BufferedReader(new FileReader(CLASSIFIED_RESULT))) {
-	    // ignore first line
-	    String dummyString = reader.readLine();
-	    String line = reader.readLine();
-	    String[] splitted = line.split(" ");
-	    Double classDouble = Double.parseDouble(splitted[0]);
-	    controllerClass = classDouble.intValue();
-	    classificationCertainty = Double.parseDouble(splitted[classificationCertainyIndexMap.get(controllerClass)]);
-	}
-	catch (Exception e) {
-	    System.out.println(String.format("Got Exception: %s", e));
-	}
-	System.out.println("Choosing controller " + controllersMap.get(controllerClass) + " with certainty "
-			   + classificationCertainty *100+"%");
-	switch (controllerClass) {
-	    case 0:
-		chosenAgent = new controllers.singlePlayer.YOLOBOT.Agent(so, elapsedTimer);
-		break;
-	    case 1:
-		chosenAgent = new controllers.singlePlayer.YBCriber.Agent(so, elapsedTimer);
-		break;
-	    case 2:
-		chosenAgent = new controllers.singlePlayer.thorbjrn.Agent(so, elapsedTimer);
-		break;
-	    case 3:
-		chosenAgent = new controllers.singlePlayer.NovTea.Agent(so, elapsedTimer);
-		break;
-	    default:
-		System.out.println("Class was not in range.");
-		break;
-	}
+        // Ensure ordering is correct
+        StringBuilder builder = new StringBuilder();
+        builder.append("{");
+        builder.append("numTypesResources=").append(features.get("numTypesResources")).append(", ");
+        builder.append("numNPCTypes=").append(features.get("numNPCTypes")).append(", ");
+        builder.append("blockSize=").append(features.get("blockSize")).append(", ");
+        builder.append("numPortals=").append(features.get("numPortals")).append(", ");
+        builder.append("isUseAvailable=").append(features.get("isUseAvailable")).append(", ");
+        builder.append("canMoveVertically=").append(features.get("canMoveVertically")).append("}\n");
+
+        String featureString = builder.toString();
+        System.out.println(featureString);
+
+        // write features to file for classification
+        // TODO check that the ordering of features is correct
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(FEATURES_FILENAME), "utf-8")))
+        {
+            // write arbitrary number larger than number of classes for SVM to not take this as a training example
+            writer.write(ARBITRARY_NUMBER + " ");
+            String[] splittedOnSpace = featureString.split(" ");
+            for (int i = 0; i < features.size(); i++) {
+                String[] splittedOnEqual = splittedOnSpace[i].split("=");
+                String[] splittedOnDot = splittedOnEqual[1].split("\\.");
+                String result = splittedOnDot[0];
+                if(i == 0) writer.write((i+1)+ ":" + result);
+                else writer.write(" "+ (i+1) + ":" + result);
+            }
+        }
+        catch (Exception e){
+            System.out.println(String.format("Got Exception: %s", e));
+        }
+        svm_predict.main(new String[]{ "-b", "1", FEATURES_FILENAME, SVM_MODEL_FILENAME, CLASSIFIED_RESULT });
+        int controllerClass = -1;
+        double classificationCertainty = -1.0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(CLASSIFIED_RESULT))) {
+            // ignore first line
+            String dummyString = reader.readLine();
+            String line = reader.readLine();
+            String[] splitted = line.split(" ");
+            Double classDouble = Double.parseDouble(splitted[0]);
+            controllerClass = classDouble.intValue();
+            classificationCertainty = Double.parseDouble(splitted[classificationCertainyIndexMap.get(controllerClass)]);
+        }
+        catch (Exception e) {
+            System.out.println(String.format("Got Exception: %s", e));
+        }
+        System.out.println("Choosing controller " + controllersMap.get(controllerClass) + " with certainty "
+                + classificationCertainty *100+"%");
+        switch (controllerClass) {
+            case 0:
+                chosenAgent = new controllers.singlePlayer.YOLOBOT.Agent(so, elapsedTimer);
+                break;
+            case 1:
+                chosenAgent = new controllers.singlePlayer.YBCriber.Agent(so, elapsedTimer);
+                break;
+            case 2:
+                chosenAgent = new controllers.singlePlayer.thorbjrn.Agent(so, elapsedTimer);
+                break;
+            case 3:
+                chosenAgent = new controllers.singlePlayer.NovTea.Agent(so, elapsedTimer);
+                break;
+            default:
+                System.out.println("Class was not in range.");
+                break;
+        }
     }
 
     /**
@@ -125,7 +132,7 @@ public class Agent extends AbstractPlayer {
      * @return An action for the current state
      */
     @Override public ACTIONS act(final StateObservation stateObs, final ElapsedCpuTimer elapsedTimer) {
-	return chosenAgent.act(stateObs, elapsedTimer);
+        return chosenAgent.act(stateObs, elapsedTimer);
     }
 
     /**
@@ -137,7 +144,7 @@ public class Agent extends AbstractPlayer {
     public void result(StateObservation stateObservation, ElapsedCpuTimer elapsedCpuTimer)
     {
 //        System.out.println("MCTS avg iters: " + SingleMCTSPlayer.iters / SingleMCTSPlayer.num);
-	//Include your code here to know how it all ended.
-	//System.out.println("Game over? " + stateObservation.isGameOver());
+        //Include your code here to know how it all ended.
+        //System.out.println("Game over? " + stateObservation.isGameOver());
     }
 }
